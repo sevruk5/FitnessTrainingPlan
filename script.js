f1075e00cabbe3f8dd44c80c6cd7607b120c5db1
sevruk5 <103356785+sevruk5@users.noreply.github.com>
2022-04-10 22:05:52 +0300

    FormsIsWork

--- script.js
+++ script.js
@@ -1,5 +1,30 @@
-document.forms.main.onsubmit = function() {
-  var message = this.inputDuration.value;
-  console.log(message)
-  return false;
-};
+let duration;
+let km;
+let pulse;
+
+function Complete()
+{
+	km = document.main.km.value;
+	averagePulse = document.main.averagePulse.value;
+
+	addRow('inputTable');
+}
+
+function addRow(tableID) {
+  // Get a reference to the table
+  var tableRef = document.getElementById(tableID);
+
+  // Insert a row in the table at row index 0
+  var newRow = tableRef.insertRow();
+
+  // Insert a cell in the row at index 0
+  var newCell1 = newRow.insertCell(0);
+  var newCell2 = newRow.insertCell(1);
+
+  // Append a text node to the cell
+  var newText1 = document.createTextNode(km);
+  newCell1.appendChild(newText1);
+
+  var newText2 = document.createTextNode(averagePulse);
+  newCell2.appendChild(newText2);
+}
