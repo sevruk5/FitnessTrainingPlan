ad955584c565ffdbba5f9a8c33cd3de6ae893f53
sevruk5 <103356785+sevruk5@users.noreply.github.com>
2022-04-15 22:28:56 +0300

    Add record in 2 table without class

--- script.js
+++ script.js
@@ -1,50 +1,85 @@
-let km;
-let averagePulse;
+function addRecordInTableNewTraining() {
+	let tableNewTraining = document.getElementById('tableNewTraining');
 
-function addInTable()
-{
-	km = document.main.km.value;
-	averagePulse = document.main.averagePulse.value;
+	let newRow = tableNewTraining.insertRow();
 
-	addRow('inputTable');
+	let newCellKm = newRow.insertCell(0);
+	let enteredKm = document.formAddNewTraining.enteredKm.value;
+	newCellKm.innerHTML = enteredKm;
+
+	let newCellAveragePulse = newRow.insertCell(1);
+	let enteredAveragePulse = document.formAddNewTraining.enteredAveragePulse.value;
+	newCellAveragePulse.innerHTML = enteredAveragePulse;
 }
 
-function addRow(tableID) {
-  // Get a reference to the table
-  var tableRef = document.getElementById(tableID);
-
-  // Insert a row in the table at row index 0
-  var newRow = tableRef.insertRow();
-
-  // Insert a cell in the row at index 0
-  var newCell1 = newRow.insertCell(0);
-  var newCell2 = newRow.insertCell(1);
-
-  // Append a text node to the cell
-  var newText1 = document.createTextNode(km);
-  newCell1.appendChild(newText1);
-
-  var newText2 = document.createTextNode(averagePulse);
-  newCell2.appendChild(newText2);
+function addRecordInTableAllTraining()
+{
+	let tableAllTraining = document.getElementById('tableAllTraining');
+	let tableNewTraining = document.getElementById('tableNewTraining');
+	let countCellsTableAllTraining = tableAllTraining.rows[0].cells.length;
+	let countRowsTableNewTraining = tableNewTraining.rows.length;
+	let countRowsTableAllTraining = tableAllTraining.rows.length;
+	let newRow = addNewRow(tableAllTraining);
+	insertCellsInRow(newRow, countCellsTableAllTraining);
+	fillingCellsInRow(newRow, countCellsTableAllTraining, tableAllTraining, countRowsTableAllTraining);
+	deleteAllRecordInTableNewTraining(tableNewTraining, countRowsTableNewTraining)
 }
 
-function addInMainTable(tableID)
-{
-	let tableRef = document.getElementById('inputTable');
-	let countRows = tableRef.rows.length;
+function fillingCellsInRow(newRow, countCellsTableAllTraining, tableAllTraining, countRowsTableAllTraining){
+	let arrAveragePulse = [];
+	fillArrayValuesFromTableNewTraining(arrAveragePulse, countCellsTableAllTraining);
+	// for(let i=0;i<arrAveragePulse.length;i++){
+	// 	alert(arrAveragePulse[i]);
+	// }
+	for(let i = 1; i < countCellsTableAllTraining;i++){
+		tableAllTraining.rows[countRowsTableAllTraining].cells[i].innerHTML = arrAveragePulse[i];
+	}
+	let today = new Date().toISOString().split("T")[0];
+	tableAllTraining.rows[countRowsTableAllTraining].cells[0].innerHTML = today;
+}
 
-	for (var i = 1; i < countRows; i++)
+function fillArrayValuesFromTableNewTraining(arrAveragePulse, countCellsTableAllTraining){
+	fillArrayWithZero(arrAveragePulse);
+	fillArrayWithAveragePulse(arrAveragePulse, countCellsTableAllTraining);
+}
+
+function fillArrayWithAveragePulse(arrAveragePulse, countCellsTableAllTraining){
+	let tableNewTraining = document.getElementById('tableNewTraining');
+	let tableAllTraining = document.getElementById('tableAllTraining');
+	let countRowsTableNewTraining = tableNewTraining.rows.length;
+	for(let i=1;i<countCellsTableAllTraining;i++)
 	{
-		let cell1 = tableRef.rows[i].cells[0].innerHTML;
-		let cell2 = tableRef.rows[i].cells[1].innerHTML;
-
-		let tableRef2 = document.getElementById('inputTable2');
-		let newRow = tableRef2.insertRow();
-		for(let i=0;i<9;i++)
-		{
-			let newCell = newRow.insertCell(i);
-			var newText = document.createTextNode("км");
-	  		newCell.appendChild(newText);
+		for(let j = 1; j < countRowsTableNewTraining; j++){
+			let cellKm = tableNewTraining.rows[j].cells[0].innerHTML;
+			let cellAveragePulse = tableNewTraining.rows[j].cells[1].innerHTML;
+			if(cellKm==(i+4)){
+				arrAveragePulse[i] = cellAveragePulse;
+			}	
 		}
+	}
+}
+
+function addNewRow(tableAllTraining){
+	let newRow = tableAllTraining.insertRow();
+	return newRow;
+}
+
+function insertCellsInRow(newRow, countCellsTableAllTraining){
+	for(let i=0;i<countCellsTableAllTraining;i++)
+	{
+		let cell = newRow.insertCell(i);
+	}
+}
+
+function fillArrayWithZero(arrAveragePulse){
+	for(let i=0;i<9;i++)
+	{
+		arrAveragePulse[i] = 0;
+	}
+}
+
+function deleteAllRecordInTableNewTraining(tableNewTraining, countRowsTableNewTraining){
+	for(let i = 1; i<countRowsTableNewTraining;i++){
+		document.getElementById("tableNewTraining").deleteRow(1);
 	}
 }
